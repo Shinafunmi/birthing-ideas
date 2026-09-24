a56c516e316616fcd5c35bb4783fcbaac5c0d21e
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2026-09-24 09:12:53 -0700

    committing on my lazy day

--- paw-world/petMy/server/inngest/index.js
+++ paw-world/petMy/server/inngest/index.js
@@ -2,16 +2,22 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "petmy-app" });
+export const inngest = new Inngest({ 
+    id: "petmy-app"
+ });
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+
 const syncUserCreation = inngest.createFunction(
-    {id: "sync-user-from-clerk"},
-    {event: "clerk/user.created"},
-    async ({event, step}) => {
-        const {id, first_name, last_name, email_adresses, image_url} = event.data;
-        let username = email_adresses[0].email_address.split("@")[0];  
+    {
+        id: "sync-user-from-clerk",
+        triggers: {
+            event: "clerk/user.created"
+        }
+    },
+    async ({event}) => {
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
+        let username = email_addresses[0].email_address.split("@")[0];  
 
         // check if user already exists in the database
        const user = await User.findOne({username})
@@ -23,28 +29,31 @@ const syncUserCreation = inngest.createFunction(
        const userData = {
         _id: id,
         username: username,
-        email: email_adresses[0].email_address,
+        email: email_addresses[0].email_address,
         full_name: first_name + " " + last_name,
         profile_picture: image_url
        };
 
          await User.create(userData);
-      
-
        
     }
 );
 
-// ingest function to sync user creation from clerk to our database
+// ingest function to sync user updation from clerk to our database
 
 const syncUserUpdation = inngest.createFunction(
-    {id: "sync-user-from-clerk"},
-    {event: "clerk/user.updated"},
-    async ({event, step}) => {
-        const {id, first_name, last_name, email_adresses, image_url} = event.data;
+    {
+        id: "sync-user-from-clerk-update",
+        triggers: {
+            event: "clerk/user.updated"
+        }
+    },
+    
+    async ({event}) => {
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
        
         const updatedUserData = {
-            email: email_adresses[0].email_address,
+            email: email_addresses[0].email_address,
             full_name: first_name + " " + last_name,
             profile_picture: image_url
         };
@@ -56,9 +65,13 @@ const syncUserUpdation = inngest.createFunction(
 // ingest function to sync user deletions from clerk to our database
 
 const syncUserDeletion = inngest.createFunction(
-    {id: "sync-user-from-clerk"},
-    {event: "clerk/user.deleted"},
-    async ({event, step}) => {
+    {
+        id: "sync-user-from-clerk-delete",
+        triggers: {
+            event: "clerk/user.deleted"
+        }
+    },
+    async ({event}) => {
         const {id} = event.data;
         await User.findByIdAndDelete(id);
     }
--- paw-world/petMy/server/server.js
+++ paw-world/petMy/server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import 'dotenv/config.js';
 import connectDB from './configs/db.js';
 import {inngest, functions} from './inngest/index.js';
-import { server } from 'inngest/express';
+import { serve } from 'inngest/express';
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Server is running'));
-app.use('/api/inngest', serve({client: inngest, functions}));
+app.use('/api/inngest', serve({client: inngest, functions: functions }));
 
 const PORT = process.env.PORT || 5000;
 
